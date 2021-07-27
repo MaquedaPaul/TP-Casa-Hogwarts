47aff77f31272ace49761db408e7095163c080b3
Maqueda Paul <maquedapablodavid@gmail.com>
2021-07-27 17:10:39 -0300

    Correciones es/1.

--- src/sombreroSeleccionador.pl
+++ src/sombreroSeleccionador.pl
@@ -2,32 +2,39 @@
 %% Parte 1 - Sombrero Seleccionador
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+es/2.
+caracter/1.
+odiaCasa/2.
+casa/1.
+mago/1.
+sombrero/2.
+permiteEntrar/2.
+tieneCaracterPara/2.
+podriaQuedarEn/2.
+cadenaDeAmistades/1.
 
+es(_,Mago):-
+    mago(Mago).
 
-esMestizo(harry).
-esCorajudo(harry).
-esAmistoso(harry).
-esOrgulloso(harry).
-esInteligente(harry).
+%Harry
+es(corajudo,harry).
+es(amistoso,harry).
+es(orgulloso,harry).
+es(inteligente,harry).
+es(mestizo,harry).
 odiaCasa(harry,slytherin).
 
-esMestizo(harry).
-esCorajudo(harry).
-esAmistoso(harry).
-esOrgulloso(harry).
-esInteligente(harry).
-odiaCasa(harry,slytherin).
-
-esPuro(draco).
-esOrgulloso(draco).
-esInteligente(draco).
+%Draco
+es(orgulloso,draco).
+es(inteligente,draco).
+es(puro,draco).
 odiaCasa(draco,hufflepuff).
 
-esImpuro(hermione).
-esOrgulloso(hermione).
-esInteligente(hermione).
-esResponsable(hermione).
-
+%Hermione
+es(orgulloso,hermione).
+es(inteligente,hermione).
+es(impuro,hermione).
+es(responsable,hermione).
 
 casa(gryffindor).
 casa(ravenclaw).
@@ -38,28 +45,29 @@ mago(harry).
 mago(draco).
 mago(hermione).
 
-sombrero/2.
+
 
 sombrero(gryffindor,Mago):-
-    esCorajudo(Mago).
+    es(corajudo,Mago).
 
 sombrero(slytherin,Mago):-
-    esInteligente(Mago),
-    esOrgulloso(Mago).
+    es(inteligente,Mago),
+    es(orgulloso,Mago).
 
 sombrero(ravenclaw,Mago):-
-    esInteligente(Mago),
-    esResponsable(Mago).
+    es(inteligente,Mago),
+    es(responsable,Mago).
 
 sombrero(hufflepuff,Mago):-
-    esAmistoso(Mago).
+    es(amistoso,Mago).
 
 permiteEntrar(Mago,Casa):-
     mago(Mago),
-    casa(Casa).
+    casa(Casa),
+    Casa \= slytherin.
 
 permiteEntrar(Mago,slytherin):-
-    not(esImpuro(Mago)).
+    not(es(impuro,Mago)).
 
 tieneCaracterPara(Casa,Mago):-
     sombrero(Casa,Mago).
@@ -71,10 +79,8 @@ podriaQuedarEn(Mago,Casa):-
 
 podriaQuedarEn(hermione,gryffindor).
 
-cadenaDeAmistades/1.
-
 cadenaDeAmistades(Magos):-
-    forall(member(Mago,Magos),esAmistoso(Mago)),
+    forall(member(Mago,Magos),es(amistoso,Mago)),
     nth1(Posicion,Mago,Magos),
     nth1(Posicion+1,Mago2,Magos),
     podriaQuedarEn(Mago,Casa),
