f40649104b068743bed251ebf812995fec2e2f68
Maqueda Paul <maquedapablodavid@gmail.com>
2021-07-27 02:26:57 -0300

    Base de conocimientos

--- src/sombreroSeleccionador.pl
+++ src/sombreroSeleccionador.pl
@@ -4,9 +4,45 @@
 
 
 
+esMestizo(harry).
+esCorajudo(harry).
+esAmistoso(harry).
+esOrgulloso(harry).
+esInteligente(harry).
+odiaCasa(harry,slytherin).
 
+esMestizo(harry).
+esCorajudo(harry).
+esAmistoso(harry).
+esOrgulloso(harry).
+esInteligente(harry).
+odiaCasa(harry,slytherin).
 
+esPuro(draco).
+esOrgulloso(draco).
+esInteligente(draco).
+odiaCasa(draco,hufflepuff).
 
+esImpuro(hermione).
+esOrgulloso(hermione).
+esInteligente(hermione).
+esResponsable(hermione).
+
+sombrero/2.
+
+sombrero(gryffindor,Mago):-
+    esCorajudo(Mago).
+
+sombrero(slytherin,Mago):-
+    esInteligente(Mago),
+    esOrgulloso(Mago).
+
+sombrero(ravenclaw,Mago):-
+    esInteligente(Mago),
+    esResponsable(Mago).
+
+sombrero(hufflepuff,Mago):-
+    esAmistoso(Mago).
 
 
 
