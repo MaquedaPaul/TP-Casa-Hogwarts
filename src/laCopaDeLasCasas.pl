119d7ab87211a8010df67f217fa6b488397b977f
Maqueda Paul <maquedapablodavid@gmail.com>
2021-08-02 00:17:35 -0300

    ClaCopaDeLasCasas.pl [Seteado]

--- src/laCopaDeLasCasas.pl
+++ src/laCopaDeLasCasas.pl
@@ -1,3 +1,33 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Parte 2 - La copa de las casas
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+esDe(hermione, gryffindor).
+esDe(ron, gryffindor).
+esDe(harry, gryffindor).
+esDe(draco, slytherin).
+esDe(luna, ravenclaw).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
